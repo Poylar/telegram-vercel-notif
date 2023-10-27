8164b2bb3dba6cbc5238e1e653437aae00bdf748
Poylar <doaleks8@gmail.com>
2023-10-28 04:34:52 +0800

    fix json

--- app/api/route.js
+++ app/api/route.js
@@ -7,16 +7,14 @@ export const runtime = 'nodejs'
 
 export async function POST(req, res) {
 	try {
-		const { message } = await req.json()
+		const message = await req.json()
 
 		// Проверяем, является ли message строкой, если нет, преобразуем его
-		const messageText =
-			typeof message === 'string' ? message : JSON.stringify(message)
 
-		await bot.telegram.sendMessage(885984456, messageText)
+		await bot.telegram.sendMessage(885984456, JSON.stringify(message))
 		return NextResponse.json({ success: 'kk' })
 	} catch (error) {
 		console.error(error)
-		return NextResponse.json({ error: 'An error occurred' })
+		return NextResponse.json({ error: error })
 	}
 }
